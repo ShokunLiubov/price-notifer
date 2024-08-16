2bf20e4e7a1463f3b0d55e1e1490efddfebb9b96
Shokun Liubov <tertyshnaya99love@gmail.com>
2024-08-16 15:34:56 +0300

    fix request

--- src/Core/Request/Request.php
+++ src/Core/Request/Request.php
@@ -6,37 +6,37 @@ namespace App\Core\Request;
 
 class Request
 {
-    public static function get(string $key, mixed $default = null): mixed
+    public function get(string $key, mixed $default = null): mixed
     {
         return $_GET[$key] ?? $default;
     }
 
-    public static function post(string $key, mixed $default = null): mixed
+    public function post(string $key, mixed $default = null): mixed
     {
         return $_POST[$key] ?? $default;
     }
 
-    public static function request(string $key, mixed $default = null): mixed
+    public function request(string $key, mixed $default = null): mixed
     {
         return $_REQUEST[$key] ?? $default;
     }
 
-    public static function server(string $key, mixed $default = null): mixed
+    public function server(string $key, mixed $default = null): mixed
     {
         return $_SERVER[$key] ?? $default;
     }
 
-    public static function cookie(string $key, mixed $default = null): mixed
+    public function cookie(string $key, mixed $default = null): mixed
     {
         return $_COOKIE[$key] ?? $default;
     }
 
-    public static function session(string $key, mixed $default = null): mixed
+    public function session(string $key, mixed $default = null): mixed
     {
         return $_SESSION[$key] ?? $default;
     }
 
-    public static function files(string $key, mixed $default = null): mixed
+    public function files(string $key, mixed $default = null): mixed
     {
         return $_FILES[$key] ?? $default;
     }
